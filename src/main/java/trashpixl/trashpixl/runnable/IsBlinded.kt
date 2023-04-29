507ff2cf7da4b2af73ea49351e6299a674c1be9f
Thierry Lachance <101730754+TLachancearduino@users.noreply.github.com>
2023-04-29 10:19:04 -0400

    fix a small bug affecting game 11

--- src/main/java/trashpixl/trashpixl/runnable/IsBlinded.kt
+++ src/main/java/trashpixl/trashpixl/runnable/IsBlinded.kt
@@ -3,9 +3,10 @@ package trashpixl.trashpixl.runnable
 import org.bukkit.potion.PotionEffectType
 
 fun isBlinded() {
-    val effect = Variable.finder?.getPotionEffect(PotionEffectType.BLINDNESS)
-    if(effect != null && minigame() == 11){
-        Variable.isBlindFinder = false
+    val effect = Variable.finder?.getPotionEffect(PotionEffectType.BLINDNESS)//store the blindness potion effect
+    if(effect == null && minigame() == 11){//check if effect isn't null and the ongoing minigame is the 11
+        Variable.isBlindFinder = false//set id blind to false
+
     }
 
 }
--- src/main/java/trashpixl/trashpixl/runnable/LineOfSight.kt
+++ src/main/java/trashpixl/trashpixl/runnable/LineOfSight.kt
@@ -4,10 +4,10 @@ import org.bukkit.Bukkit
 
 
 fun lineOfSight(){
-    if(minigame() == 11) {
-        Bukkit.getOnlinePlayers().forEach { player ->
-            if (player != Variable.finder && Variable.finder!!.hasLineOfSight(player) && !Variable.isBlindFinder) {
-                player.health = 0.0
+    if(minigame() == 11) {//check if the ongoing minigame is the 11
+        Bukkit.getOnlinePlayers().forEach { player ->//run this for each player for ever
+            if (player != Variable.finder && Variable.finder!!.hasLineOfSight(player) && !Variable.isBlindFinder) {//check if itd id not the finder check if the finder see him and if the finder is blind
+                player.health = 0.0//set player health to zero
             }
         }
 
