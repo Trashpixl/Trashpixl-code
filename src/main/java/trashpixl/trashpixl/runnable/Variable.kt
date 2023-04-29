4da360e58b0d8494030fcc8a7a10b60b267f9b41
Thierry Lachance <101730754+TLachancearduino@users.noreply.github.com>
2023-04-29 09:38:00 -0400

    update some comment and the jar install path

--- src/main/java/trashpixl/trashpixl/runnable/Variable.kt
+++ src/main/java/trashpixl/trashpixl/runnable/Variable.kt
@@ -14,8 +14,8 @@ class Variable {//create the variable class
         var time: LocalTime? = null//store the time for the timer
         var playerArrayNumber: Int = 0//store which player we are at for the array
         var playerArray: MutableList<String>? = null//store the name of all player
-        var finder: Player? = null
-        var isBlindFinder = true
-        var tag: Player? = null
+        var finder: Player? = null//store the finder for hide ans seek
+        var isBlindFinder = true//var to store the state of a player view
+        var tag: Player? = null//store the tag for tag player
     }
 }
