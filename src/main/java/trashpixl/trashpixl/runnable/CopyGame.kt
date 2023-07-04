24bdf8233377fd80863ea8b86cc59aba5942bf41
Thierry Lachance <lachancethierry6@gmail.com>
2023-07-04 16:21:21 -0400

    start modifing the code for worldedit
    Signed-off-by: Thierry Lachance <lachancethierry6@gmail.com>

---
+++ src/main/java/trashpixl/trashpixl/runnable/CopyGame.kt
@@ -0,0 +1,136 @@
+package trashpixl.trashpixl.runnable
+
+import org.bukkit.Bukkit
+
+fun copyGame(game: String){
+    if(game == "tnt"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "tag"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "searchAndFind"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "hideAndSeek"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "pvpSumo"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "pvp"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "parkour"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "pvpBow"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "pvpKnockBack"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "pvpLoot"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "DodgeBall"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "button4"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+    if(game == "bridgeRace"){
+        val x1 = 0.0
+        val y1 = 0.0
+        val z1 = 0.0
+
+        val x2 = 0.0
+        val y2 = 0.0
+        val z2 = 0.0
+        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+    }
+}
