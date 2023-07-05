3eae757a69df98e0b9808f57830d9d5ba52e273a
Thierry Lachance <lachancethierry6@gmail.com>
2023-07-05 17:07:34 -0400

    finished the copygame function now we just need to integrate it
    Signed-off-by: Thierry Lachance <lachancethierry6@gmail.com>

--- src/main/java/trashpixl/trashpixl/runnable/CopyGame.kt
+++ src/main/java/trashpixl/trashpixl/runnable/CopyGame.kt
@@ -1,8 +1,28 @@
 package trashpixl.trashpixl.runnable
 
+
+
+import com.sk89q.worldedit.WorldEdit
+import com.sk89q.worldedit.WorldEditException
+import com.sk89q.worldedit.bukkit.BukkitAdapter
+import com.sk89q.worldedit.function.operation.ForwardExtentCopy
+import com.sk89q.worldedit.function.operation.Operations
+import com.sk89q.worldedit.math.BlockVector3
+import com.sk89q.worldedit.regions.CuboidRegion
 import org.bukkit.Bukkit
 
+
 fun copyGame(game: String){
+    val xFrom = 0.0
+    val yFrom = 0.0
+    val zFrom = 0.0
+
+    val xTo = 0.0
+    val yTo = 0.0
+    val zTo = 0.0
+    val vectorFrom = BlockVector3.at(xFrom, yFrom, zFrom)
+    val vectorTo = BlockVector3.at(xTo, yTo, zTo)
+
     if(game == "tnt"){
         val x1 = 0.0
         val y1 = 0.0
@@ -11,7 +31,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "tag"){
         val x1 = 0.0
@@ -21,7 +45,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "searchAndFind"){
         val x1 = 0.0
@@ -31,7 +59,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "hideAndSeek"){
         val x1 = 0.0
@@ -41,7 +73,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "pvpSumo"){
         val x1 = 0.0
@@ -51,7 +87,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "pvp"){
         val x1 = 0.0
@@ -61,7 +101,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "parkour"){
         val x1 = 0.0
@@ -71,7 +115,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "pvpBow"){
         val x1 = 0.0
@@ -81,7 +129,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "pvpKnockBack"){
         val x1 = 0.0
@@ -91,7 +143,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
     }
     if(game == "pvpLoot"){
         val x1 = 0.0
@@ -101,7 +157,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "DodgeBall"){
         val x1 = 0.0
@@ -111,7 +171,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "button4"){
         val x1 = 0.0
@@ -121,7 +185,11 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
     if(game == "bridgeRace"){
         val x1 = 0.0
@@ -131,6 +199,24 @@ fun copyGame(game: String){
         val x2 = 0.0
         val y2 = 0.0
         val z2 = 0.0
-        Bukkit.getServer().dispatchCommand(Bukkit.getServer().consoleSender, "")
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+
     }
 }
+fun copyRegion(min: BlockVector3?, max: BlockVector3?, from: BlockVector3?, to: BlockVector3?) {
+    val world = BukkitAdapter.adapt(Bukkit.getWorld("world"))
+    val region = CuboidRegion(min, max)
+    val editSession = WorldEdit.getInstance().newEditSessionBuilder().world(world).maxBlocks(-1).build()
+    val forwardExtentCopy = ForwardExtentCopy(
+            world, region, from, editSession, to
+    )
+    try {
+        Operations.complete(forwardExtentCopy)
+    } catch (e: WorldEditException) {
+        throw RuntimeException(e)
+    }
+}
+
