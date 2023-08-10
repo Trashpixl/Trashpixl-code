6dc8fff3c42273629890f614ebef6863b5ea1bbf
thierry-lachance <lachancethierry6@gmail.com>
2023-08-10 13:13:19 -0400

    add the coord the game copy

--- src/main/java/trashpixl/trashpixl/runnable/CopyGame.kt
+++ src/main/java/trashpixl/trashpixl/runnable/CopyGame.kt
@@ -13,210 +13,220 @@ import org.bukkit.Bukkit
 
 
 fun copyGame(game: String){//TODO: add the coords
-    val xFrom = 0.0
-    val yFrom = 0.0
-    val zFrom = 0.0
-
+    //todo add something to clear the previous map
     val xTo = 0.0
     val yTo = 0.0
     val zTo = 0.0
-    val vectorFrom = BlockVector3.at(xFrom, yFrom, zFrom)
     val vectorTo = BlockVector3.at(xTo, yTo, zTo)
 
     if(game == "tnt"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 282.0
+        val y1 = -32.0
+        val z1 = 279.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 245.0
+        val y2 = -64.0
+        val z2 = 245.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "tag"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 288.0
+        val y1 = -32.0
+        val z1 = 339.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 400.0
+        val y2 = -64.0
+        val z2 = 400.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "searchAndFind"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 288.0
+        val y1 = -32.0
+        val z1 = 339.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 400.0
+        val y2 = -64.0
+        val z2 = 400.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "hideAndSeek"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 288.0
+        val y1 = -32.0
+        val z1 = 339.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 400.0
+        val y2 = -64.0
+        val z2 = 400.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "pvpSumo"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 245.0
+        val y1 = -32.0
+        val z1 = 280.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 195.0
+        val y2 = -64.0
+        val z2 = 220.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "pvp"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 245.0
+        val y1 = -32.0
+        val z1 = 280.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 195.0
+        val y2 = -64.0
+        val z2 = 220.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "parkour"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 227.0
+        val y1 = -32.0
+        val z1 = 280.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 127.0
+        val y2 = -64.0
+        val z2 = 320.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "pvpBow"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 245.0
+        val y1 = -32.0
+        val z1 = 280.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 195.0
+        val y2 = -64.0
+        val z2 = 220.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "pvpKnockBack"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 245.0
+        val y1 = -32.0
+        val z1 = 280.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 195.0
+        val y2 = -64.0
+        val z2 = 220.0
 
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
     }
     if(game == "pvpLoot"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 118.0
+        val y1 = -32.0
+        val z1 = 280.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 138.0
+        val y2 = -64.0
+        val z2 = 259.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "DodgeBall"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 343.0
+        val y1 = -32.0
+        val z1 = 282.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 292.0
+        val y2 = -64.0
+        val z2 = 333.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "button4"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 181.0
+        val y1 = -32.0
+        val z1 = 263.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 195.0
+        val y2 = -64.0
+        val z2 = 280.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "bridgeRace"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 180.0
+        val y1 = -64.0
+        val z1 = 259.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 138.0
+        val y2 = -32.0
+        val z2 = 280.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
     if(game == "camelFight"){
-        val x1 = 0.0
-        val y1 = 0.0
-        val z1 = 0.0
+        val x1 = 128.0
+        val y1 = -31.0
+        val z1 = 240.0
 
-        val x2 = 0.0
-        val y2 = 0.0
-        val z2 = 0.0
+        val x2 = 195.0
+        val y2 = -64.0
+        val z2 = 152.0
         val vector1 = BlockVector3.at(x1, y1, z1)
         val vector2 = BlockVector3.at(x2, y2, z2)
 
-        copyRegion(vector1, vector2, vectorFrom, vectorTo)
+        copyRegion(vector1, vector2, vector1, vectorTo)
+
+    }
+    if(game == "Labi"){
+        val x1 = 227.0
+        val y1 = -31.0
+        val z1 = 280.0
+
+        val x2 = 289.0
+        val y2 = -64.0
+        val z2 = 333.0
+        val vector1 = BlockVector3.at(x1, y1, z1)
+        val vector2 = BlockVector3.at(x2, y2, z2)
+
+        copyRegion(vector1, vector2, vector1, vectorTo)
 
     }
 }
